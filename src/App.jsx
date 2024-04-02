7d3ad4c87dc7bef7b48c3090e589d451aa653548
Terrence Murray <52611990+TerrenceMurray@users.noreply.github.com>
2024-04-02 13:56:28 -0400

    Test Deployment to Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import { useState } from "react";
+import { Button } from "./components/ui/button";
+
 export default function App ()
 {
+	const [state, setState] = useState(0);
 	return (
 		<>
 			<h1>Home</h1>
+			<p>{state}</p>
+			<Button onClick={() => setState(state + 1)}>Increment</Button>
+			<Button onClick={() => setState(state - 1)}>Decrement</Button>
 		</>
 	);
 }
