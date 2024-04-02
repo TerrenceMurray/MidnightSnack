fb39af224962ad8e5fd4b13ff49d26f055cb158e
Terrence Murray <52611990+TerrenceMurray@users.noreply.github.com>
2024-04-02 13:45:22 -0400

    Added Shadcn-ui library

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+
+            "@": path.resolve(__dirname, "./src/"),
+
+            routes: `${path.resolve(__dirname, "./src/routes/")}`,
+
+            services: `${path.resolve(__dirname, "./src/services/")}`,
+
+        }
+  }
 })
