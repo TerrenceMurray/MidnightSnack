cbf8829fb1ccf4d43987bb1cf4deb14831bfa937
Terrence Murray <52611990+TerrenceMurray@users.noreply.github.com>
2024-04-03 08:59:36 -0400

    Made Hero section more responsive for larger screens

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import Title from "../components/Title";
 import Layout from "./Layout";
 import HeroImage from "@/assets/Hero Image.png";
 import Arrow from "@/assets/arrow.svg";
+import TitleSVG from "@/assets/title.svg";
 
 export default function Home ()
 {
@@ -10,17 +11,13 @@ export default function Home ()
     return (
         <Layout>
             <main className="px-32 flex w-full flex-1 items-center">
-                <section className="w-[29rem]">
-                    <h1 className="heading relative">
-                        Order Your <br />
-                        Favourite Foods <br />
-                        & Fast Delivery
-                        <img src={Arrow} className="absolute -right-[30%] -top-5 w-60" alt="" />
-                        <span className="block absolute bg-accent-surface w-1/2 h-[1.3rem] -z-50 bottom-3 right-9"></span>
+                <section className="inline">
+                    <h1>
+                        <img className="flex-1 h-auto w-auto max-w-full" src={TitleSVG} aria-label="" alt="Hero section title" />
                     </h1>
-                    <p className="subtitle mt-6 w-11/12">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam aliquam  ipsum risus, ac porta sapien congue a.</p>
+                    <p className="subtitle mt-6 w-[75%]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam aliquam  ipsum risus, ac porta sapien congue a.</p>
                 </section>
-                <img className="ml-auto w-[50%]" src={HeroImage} alt="Home" />
+                <img className="flex-1 h-full w-auto max-w-full" src={HeroImage} alt="Home" />
             </main>
         </Layout>
     );
