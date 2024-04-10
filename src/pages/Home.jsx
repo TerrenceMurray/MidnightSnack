48ee508103f61093223dbc6628c6e653b6f33cc8
matt8142 <matthew8142ramnath@gmail.com>
2024-04-09 23:18:30 -0400

    home updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,7 +24,7 @@ export default function Home ()
         <main className="flex w-full flex-1 justify-between items-center">
             <section className="">
                 <HomeTitle />
-                <p className="subtitle mt-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam aliquam  ipsum risus, ac porta sapien congue a.</p>
+                <p className="subtitle mt-6">From craving to table, our app makes food ordering easy, quick, and delicious!</p>
                 <div className="flex gap-4 items-center mt-10">
                     <AddressInput className="max-w-96" ref={searchInput} onChange={onChangeHandler} />
                     <Button onClick={onClickHandler} variant="cta" className="flex-1 py-4 h-auto flex gap-4"><i className="bi bi-search"></i> Search</Button>
